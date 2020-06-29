9d04c8d1cbcf1d1f4f72236d5e587753d06e6d79
Prasad Sriramula <prasad.sriramula@thebioagency.com>
2020-06-29 07:16:29 +0100

    Added spring boot boot strap using spring initializr

---
+++ src/test/java/com/opsly/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opsly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
